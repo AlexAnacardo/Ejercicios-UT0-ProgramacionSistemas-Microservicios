71aabbc87c35be2a6ebf438bb8b6b671b9279160
Alex <alexasensio04clase@gmail.com>
2025-09-22 11:05:02 +0200

    Ej 2 semi acabado

---
+++ INCOMPLETOO DAM2_PSYP_EJER0001_02_ASENSIO_SÁNCHEZ_ALEX.c
@@ -0,0 +1,29 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int numero1;
+    int numero2;
+    
+    printf("Introduce un numero:\n");
+    scanf("%d", &numero1);
+    
+    printf("Introduce otro numero:\n");
+    scanf("%d", &numero2);
+    
+    printf("La dirección de memoria del primer numero es: %p\n", (void*)&numero1);
+    
+    printf("La dirección de memoria del segundo numero es: %p\n", (void*)&numero2);
+    
+    printf("La variable 1 ocupa %d bytes de tamaño en memoria\n", sizeof(numero1));
+
+    return 0;
+}
